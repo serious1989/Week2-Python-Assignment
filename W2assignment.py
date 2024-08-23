e3f65889355824abc39217f0883fe69127412c82
Francis Ennin <enninsolve@gmail.com>
2024-08-23 16:32:28 +0530

    first commit

---
+++ W2assignment.py
@@ -0,0 +1,25 @@
+
+# Create an Empty list call My_list
+My_list = []
+
+# Append the following elements to my_list: 10, 20, 30, 40.
+My_list.append(10)
+My_list.append(20)
+My_list.append(30)
+My_list.append(40)
+
+# Insert the value 15 at the second position in the list.
+My_list.insert(1, 15)
+
+# Extend my_list with another list: [50, 60, 70].
+My_list.extend([50, 60, 70])
+
+# Remove the last element from my_list.
+My_list.pop()
+
+# Sort my_list in ascending order.
+My_list.sort()
+
+# Find and print the index of the value 30 in my_list.
+index_of_30 = My_list.index(30)
+print("The Index of 30 is", index_of_30)
